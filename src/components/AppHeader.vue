a5cad60a64df10d85d1cd570c98fe1ad3aa93829
Rizwan Zaheer <rizwanzaheer003@gmail.com>
2018-09-02 15:57:01 +0500

    adding header menu

--- src/components/AppHeader.vue
+++ src/components/AppHeader.vue
@@ -1,6 +1,22 @@
 <template>
-  <div>
-    Image Storage
+  <div class="ui secondary pointing menu">
+    <a class="item">
+      Image Storage
+    </a>
+    <a class="item active">
+      Home
+    </a>
+    <a class="item">
+      Messages
+    </a>
+    <a class="item">
+      Friends
+    </a>
+    <div class="right menu">
+      <a class="ui item">
+        Logout
+      </a>
+    </div>
   </div>
 </template>
 <script>
