c101551c120e3f45356db0171721edb91e8692c7
Rizwan Zaheer <rizwanzaheer003@gmail.com>
2018-11-09 15:09:52 +0500

    adding auth module

---
+++ src/store/modules/auth.js
@@ -0,0 +1,14 @@
+const state = {
+  token: null
+};
+const getters = {
+  isLoggedIn: state => !!state.token
+};
+const actions = {
+
+};
+const mutations = {
+  setToken: (state, token) => {
+    state.token = token;
+  }
+};
