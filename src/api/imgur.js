5b3efa9e95ad77955130bf43156c91588137c686
Rizwan Zaheer <rizwanzaheer003@gmail.com>
2018-11-10 23:11:43 +0500

    fetching images

--- src/api/imgur.js
+++ src/api/imgur.js
@@ -1,4 +1,6 @@
 import qs from 'qs';
+import axios from 'axios';
+
 const CLIENT_ID = 'd56d002712508d0';
 const ROOT_URL = 'https://api.imgur.com';
 
@@ -9,5 +11,8 @@ export default {
       response_type: 'token'
     };
     window.location = `${ROOT_URL}/oauth2/authorize?${qs.stringify(querystring)}`;
+  },
+  fetchImages() {
+    return axios.get(`${ROOT_URL}/3/account/me/images`);
   }
 };
