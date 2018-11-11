00d02c8759fa4f6119a0ba238f27be0d4b3cfe4f
Rizwan Zaheer <rizwanzaheer003@gmail.com>
2018-11-11 13:10:25 +0500

    adding images life cycle

--- src/components/ImageList.vue
+++ src/components/ImageList.vue
@@ -4,8 +4,14 @@
   </div>
 </template>
 <script>
+import { mapActions } from "vuex";
+
 export default {
-  name: "ImageList"
+  name: "ImageList",
+  methods: mapActions(["fetchImages"]),
+  created() {
+    this.fetchImages();
+  }
 };
 </script>
 
